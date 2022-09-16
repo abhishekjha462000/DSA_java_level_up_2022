0be60d21e6a9cf446c5efaa9b374b931f0b21e5e
Abhishek jha <46026348+sameerjha462000@users.noreply.github.com>
2022-09-16 23:11:31 +0530

    ㅤ

--- T002/Morris Traversals/002. inorder.java
+++ T002/Morris Traversals/002. inorder.java
@@ -1,26 +1,14 @@
-private static List<Integer> preOrder(Node node){
+private static List<Integer> inOrder(Node node){
         List<Integer> ans = new ArrayList<>();
 
         while(node != null){
-            /**
-             * If the left of the node is null, then that means this node cannot be visited twice(why?)
-             * So we just add it to the "ans" and move to the right of this node
-             **/
 
             if(node.left == null){
                 // just add to the "ans" and move to the right
                 ans.add(node.data);
                 node = node.right;
             }else{
-                // Here "node" has non-null left.
-                /**
-                 * Now here we can have two cases
-                 * We go to the rightmost node of the left subtree and check if it is already connected to the node or not.
-                 * If it is, then we need to disconnect it and move to the right subtree.
-                 * If it is not connected already, then we need to connect it to the "node" and then we move to the left of the "node"
-                 **/
-
-                Node nodep1 = node.left; // This is the left of the "node"
+                Node nodep1 = node.left;
 
                 // Move to the rightmost node
                 while(nodep1.right != null && nodep1.right != node){
@@ -28,13 +16,12 @@ private static List<Integer> preOrder(Node node){
                 }
 
                 if(nodep1.right == null){
-                    // This means that we are visiting "node" for the first time
                     nodep1.right = node;
                     node = node.left;
                 }else if(nodep1.right == node){
-                    ans.add(node.data);
-                    nodep1.right = null; // disconnect
-                    node = node.right; // move to the right
+                    ans.add(node.data); // In preorder this line was in the "null" code, for inorder we just removed it from there and put it here.
+                    nodep1.right = null;
+                    node = node.right;
                 }
 
             }
@@ -44,8 +31,8 @@ private static List<Integer> preOrder(Node node){
 
 
 
-    // ROUGH WORK
 
+    // ROUGH WORK
 
 
 
