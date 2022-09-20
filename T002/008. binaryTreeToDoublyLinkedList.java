f329998bc5f4789415e8dc7e5b3ba99575256fd7
Abhishek jha <46026348+sameerjha462000@users.noreply.github.com>
2022-09-20 17:47:24 +0530

    ㅤ

--- T002/008. binaryTreeToDoublyLinkedList.java
+++ T002/008. binaryTreeToDoublyLinkedList.java
@@ -1,24 +1,38 @@
+// problem link -- https://practice.geeksforgeeks.org/problems/binary-tree-to-dll/1
+
+/**
+ * This function returns the head of the doubly linked list
+ **/
 private static Node BinaryTreeToDoublyLinkedList(Node root){
 
         if(root == null)
             return null;
 
-        Node leftAns  = BinaryTreeToDoublyLinkedList(root.left);
-        Node rightAns = BinaryTreeToDoublyLinkedList(root.right);
+        Node leftAns  = BinaryTreeToDoublyLinkedList(root.left); // make the left tree a doubly linked list
+        Node rightAns = BinaryTreeToDoublyLinkedList(root.right); // make the right tree a doubly linked list
 
+        /**
+         * "leftAns" and "rightAns" are the heads of the left and the right doubly linked list.
+         **/
+
+        // Move to the rightmost node of the doubly linked list
         while(leftAns != null && leftAns.right != null){
             leftAns = leftAns.right
         }
 
+        // connect the "rightmost" node of the "left" doubly linked list to the "root"
         if(leftAns != null)
             leftAns.right = root;
 
+        // connect "root" to the "rightmost" node of the "left" doubly linked list.
         root.left = leftAns;
         root.right = rightAns;
 
+        // connect the "rightAns" to the "root" node
         if(rightAns != null)
             rightAns.left = root;
 
+        // move the "root" to the head of the new linked list
         while(root != null && root.left != null)
             root = root.left;
 
@@ -37,4 +51,4 @@ private static Node BinaryTreeToDoublyLinkedList(Node root){
 
 
 
-    
+
