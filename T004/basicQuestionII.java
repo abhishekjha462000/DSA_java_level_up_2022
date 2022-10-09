ff5707d0c8236fe1ca00847f0920c17b0fe6d689
Abhishek jha <46026348+sameerjha462000@users.noreply.github.com>
2022-10-09 22:33:45 +0530

    ㅤ

--- T004/basicQuestionII.java
+++ T004/basicQuestionII.java
@@ -22,10 +22,27 @@
         return 32; // This means that no bit is set
     }
 
+    public static int flipsRequiredToMakeNumberEqual(int n1, int n2){
+        int count = 0;
+
+        for(int pos = 0; pos <= 31; pos++){
+            int mask = 1 << pos;
+
+            // find the digit of n1 at this pos
+            int d1 = (n1 & mask) == 0 ? 0 : 1;
+
+            // find the digit of n2 at this pos
+            int d2 = (n2 & mask) == 0 ? 0 : 1;
+
+
+            if(d1 != d2) count++; // Since digits are not same, we need to flip one bit from one of them.
+        }
+        return count;
+    }
+
 
     // ROUGH WORK
 
 
 
 
-    
