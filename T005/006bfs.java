7614db0f07f2dd9c9a44c87ec9888315a600ed7a
Abhishek jha <46026348+sameerjha462000@users.noreply.github.com>
2022-10-11 18:22:36 +0530

    ㅤ

---
+++ T005/006bfs.java
@@ -0,0 +1,42 @@
+    private static void bfs(int src, List<Edge>[] graph){
+        int nvtces = graph.length;
+        Queue<Integer> mq = new ArrayDeque<>();
+        Queue<Integer> hq = new ArrayDeque<>();
+        boolean[] vis = new boolean[nvtces];
+        mq.add(src);
+
+        // This works on rpa algorithm which means remove print and add algorithm
+        // print and add are only done for unvisited vertices and before doing this we mark the vertex as visited.
+
+        while(!mq.isEmpty()){
+            // remove print and add
+            int removed = mq.remove(); // remove
+
+
+            if(!vis[removed]){ // if unvisited then print and add the neighbours
+                vis[removed] = true;
+                System.out.print(removed + " "); // print
+
+                for(Edge e : graph[removed]){
+                    hq.add(e.nbr); // add the neighbors
+                }
+            }
+
+            if(mq.isEmpty()){
+                System.out.println();
+                Queue<Integer> temp = hq;
+                hq = mq;
+                mq = temp;
+
+            }
+
+        }
+    }
+
+
+
+    // ROUGH WORK
+
+
+
+
