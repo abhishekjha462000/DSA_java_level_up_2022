a01841916376282b6abb7d6cc52d6e8178a643ab
Abhishek jha <46026348+sameerjha462000@users.noreply.github.com>
2022-10-13 12:44:07 +0530

    ㅤ

--- T005/013perfectFriends.java
+++ T005/013perfectFriends.java
@@ -5,28 +5,28 @@ import java.util.*;
 
 class Main{
     
-    private static class Edge{
+    private static class Edge {
         int src;
         int nbr;
 
-        Edge(){}
-        Edge(int src, int nbr){
+        Edge() {}
+        Edge(int src, int nbr) {
             this.src = src;
             this.nbr = nbr;
         }
-        Edge(Edge other){
+        Edge(Edge other) {
             this.src = other.src;
             this.nbr = other.nbr;
         }
 
         @Override
-        public String toString(){
+        public String toString() {
             return src + "->" + nbr;
         }
     }
-    
-    public static void main(String[] args) throws IOException{
-        // initializeIO();
+
+    public static void main(String[] args) throws IOException {
+        initializeIO();
         Scanner scn = new Scanner(System.in);
 
         int nvtces = scn.nextInt();
@@ -34,11 +34,11 @@ class Main{
 
         List<Edge>[] graph = new ArrayList[nvtces];
 
-        for(int i = 0;i < nvtces;i++){
+        for(int i = 0;i < nvtces;i++) {
             graph[i] = new ArrayList<>();
         }
 
-        while(nedges-- != 0){
+        while(nedges-- != 0) {
             int v1 = scn.nextInt();
             int v2 = scn.nextInt();
 
@@ -49,8 +49,8 @@ class Main{
         boolean[] vis = new boolean[nvtces]; // common visited array
 
         List<Integer> sizes = new ArrayList<>(); // This will store the sizes of all the connected components
-        for(int i = 0; i < nvtces;i++){
-            if(!vis[i]){
+        for(int i = 0; i < nvtces;i++) {
+            if(!vis[i]) {
                 dfs(i, graph, vis);
 
                 sizes.add(temp);
@@ -60,8 +60,8 @@ class Main{
 
         int count = 0 ;
 
-        for(int i = 0;i < sizes.size();i++){
-            for(int j = i+1;j < sizes.size();j++){
+        for(int i = 0;i < sizes.size();i++) {
+            for(int j = i+1;j < sizes.size();j++) {
                 count += sizes.get(i) * sizes.get(j);
             }
         }
@@ -71,18 +71,18 @@ class Main{
 
     private static int temp = 0; // stores the number of vertices in a single dfs
 
-    private static void dfs(int src, List<Edge>[] graph, boolean[] vis){
+    private static void dfs(int src, List<Edge>[] graph, boolean[] vis) {
         vis[src] = true;
         temp++;
 
-        for(Edge e : graph[src]){
-            if(!vis[e.nbr]){
+        for(Edge e : graph[src]) {
+            if(!vis[e.nbr]) {
                 dfs(e.nbr, graph, vis);
             }
         }
     }
 
-    private static void initializeIO(){
+    private static void initializeIO() {
         try 
         {
             System.setIn(new FileInputStream("input.txt"));
